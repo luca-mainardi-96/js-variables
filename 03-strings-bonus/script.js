d0e6fe74fd368b5af158b4bdcb15aa894cae13e7
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-03 17:49:37 +0200

    bonus 3

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -17,7 +17,8 @@ console.log(citySubstring[1]); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = '';
+const newCity = 'Delhi'
+const cityReplaced = citySubstring[0]+' '+newCity;
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
